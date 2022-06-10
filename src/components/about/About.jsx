2de7cdc708c0165e8e670c143ba49705593e2c77
cmccr <cmccrory548@gmail.com>
2022-06-10 10:19:33 -0600

    added some more functionality to menu

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import "./about.scss"
+import React from "react";
+import "./about.scss";
 
 export default function about() {
   return (
     <div className="about" id="about">
-      <div className="about-me">
-        <h1>About</h1>
-        <p>Hello! My name is Connor McCrory and I'm a Web Developer, who specialized in: JavaScript, ReactJS, HTML, CSS, and NodeJS. I enjoy Web Development because it combines thinking while not taking away from being creative. When I'm not working, you can find me: on my computer learning, talking, playing, or anything sports; gaming with my friends, or outside paddle boarding, or anything to enjoy the outdoors.</p>
+      <div className="left">
+        <img src="assets/conn.jpeg" alt="connor-head-shot" />
+      </div>
+      <div className="right">
+        <div classname="wrapper">
+          <h2>Hello, I'm</h2>
+          <h1>Connor McCrory</h1>
+          <h3>Web Developer</h3>
+        </div>
       </div>
     </div>
-  )
+  );
 }
